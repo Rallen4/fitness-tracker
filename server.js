19cbb3c2f27597fd1758ec0e4f653f1c1ba4670f
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-11-19 11:57:42 -0800

    linked mongodb and first app.post api route

--- server.js
+++ server.js
@@ -9,3 +9,28 @@ const PORT = process.env.PORT || 3000;
 const db = require("./models");
 const { Workout, Exercise } = require("./models");
 
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useCreateIndex: true,
+    useFindAndModify:true
+});
+
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+})
